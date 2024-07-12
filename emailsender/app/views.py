23f674f98a6adff494ad1be02664aca2652584fa
mac <singh.ravneet1995@gmail.com>
2024-07-12 18:52:15 +0530

    Email send project

---
+++ emailsender/app/views.py
@@ -0,0 +1,22 @@
+# from multiprocessing import connection
+from django.shortcuts import render
+from django.core.mail import EmailMessage, get_connection
+from django.conf import settings
+from django.views.decorators.csrf import csrf_exempt
+
+
+# Create your views here.
+@csrf_exempt
+def send_email(request):
+    if request.method == "POST":
+        with get_connection(host = settings.EMAIL_HOST,port = settings.EMAIL_PORT,username = settings.EMAIL_HOST_USER,password= settings.EMAIL_HOST_PASSWORD,use_tls = settings.EMAIL_USE_TLS) as connection:
+            subject = request.POST.get("subject")
+            print(request.body)
+            email_from = settings.EMAIL_HOST_USER
+            # recipient_list = request.POST.get("email").split()  
+
+            recipient_list = [request.POST.get("email"),]
+            print(recipient_list)
+            message = request.POST.get("message")
+            EmailMessage(subject,message,email_from,recipient_list,connection=connection).send()
+    return render(request,'home.html')
